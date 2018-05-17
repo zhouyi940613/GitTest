216639db39e91dd31396046cd9a3bad1dc32ead9
GilesZhou <giles.zhou@biposervice.com>
2018-05-17 09:34:21 +0800

    Initial Commit

---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by GilesZhou on 2018/5/17.
+//  Copyright © 2018年 BIPO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
